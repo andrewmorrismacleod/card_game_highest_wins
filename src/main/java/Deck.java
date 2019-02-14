8ca67718b5123ff141a9fa2349c74dd35d4aabd1
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2019-02-14 14:30:07 +0000

    Complete populate and shuffle deck

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Deck {
+
+    private ArrayList<Card> cards;
+
+    public Deck(){
+        this.cards = new ArrayList<>();
+    }
+
+    public int getDeckSize(){
+        return this.cards.size();
+    }
+
+    public void populate(){
+
+        for(SuitType suit : SuitType.values()){
+            for(RankType rank : RankType.values()) {
+                Card card = new Card(suit, rank);
+                this.cards.add(card);
+            }
+        }
+
+    }
+
+    public ArrayList<Card> getCards(){
+        return this.cards;
+    }
+
+    public void shuffle(){
+        Collections.shuffle(this.cards);
+    }
+
+
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class DeckTest {
+
+    Deck deck;
+
+
+    @Before
+    public void before(){
+        deck = new Deck();
+    }
+
+    @Test
+    public void hasNoCards(){
+        assertEquals(0, deck.getDeckSize());
+    }
+
+    @Test
+    public void has52Cards(){
+        deck.populate();
+        assertEquals(52, deck.getDeckSize());
+        System.out.println(deck.getCards().get(13).getRank());
+        System.out.println(deck.getCards().get(13).getSuit());
+    }
+
+    @Test
+    public void canShuffleCards(){
+        deck.populate();
+        deck.shuffle();
+        System.out.println(deck.getCards().get(13).getRank());
+        System.out.println(deck.getCards().get(13).getSuit());
+    }
 }
